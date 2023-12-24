3a4506a796fb1cfceae69e1689a69aca8cb0b52b
Babin Timsina <babintimsina@gmail.com>
2023-12-24 06:14:28 -0800

    script.js

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 function followButtonClicked() {
     alert("Thanks for following!");
 }
+// JavaScript for Dark Mode Toggle
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('change', () => {
+    body.classList.toggle('dark-mode', darkModeToggle.checked);
+});
